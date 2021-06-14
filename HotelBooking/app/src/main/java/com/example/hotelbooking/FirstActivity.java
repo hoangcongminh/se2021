cffbda4219dc0abf3b69ca831e919383f93184cd
Công Minh Hoàng <you@example.com>
2021-06-14 19:17:37 +0700

    update layout

---
+++ HotelBooking/app/src/main/java/com/example/hotelbooking/FirstActivity.java
@@ -0,0 +1,28 @@
+package com.example.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.hotelbooking.Activities.MainActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button btn_book = findViewById(R.id.btn_book);
+        btn_book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
